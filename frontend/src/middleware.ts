68680fb0ae7c654918e6206759d6a6b7cca466c4
IamAmanyadav <121033170+IamAmanyadav@users.noreply.github.com>
2026-08-13 23:57:29 +0530

    Refactor middleware to include __clerk routes

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,6 +1,5 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-// Routes that anyone can access without authentication
 const isPublicRoute = createRouteMatcher([
   "/",
   "/sign-in(.*)",
@@ -12,22 +11,17 @@ const isPublicRoute = createRouteMatcher([
 ]);
 
 export default clerkMiddleware(async (auth, request) => {
-  // Allow public routes
   if (isPublicRoute(request)) {
     return;
   }
 
-  // Protect all other routes
   await auth.protect();
 });
 
 export const config = {
   matcher: [
-    // Run middleware on application routes while skipping
-    // Next.js internals and static files
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-
-    // Always run middleware for API and TRPC routes
     "/(api|trpc)(.*)",
+    "/__clerk/(.*)",
   ],
 };
